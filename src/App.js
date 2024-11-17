4fac043991b2e45228e10cc9d8277ad1092e087e
Anthai87 <haidariano@hotmail.com>
2024-11-17 18:52:51 +0100

    login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Terminal from "./Terminal";
+import LoginComponent from "./LoginComponent";
 
 function App() {
   return (
     <div className="App">
-      <Terminal />
+      <Router>
+        <Routes>
+          {/* Route for the Login page */}
+          <Route path="/" element={<LoginComponent />} />
+
+          {/* Route for the Dashboard or next page */}
+          <Route path="/terminal" element={<Terminal />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
